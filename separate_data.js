2e585e087d87275232e0daf90061c1074d4f8352
Kyle Thayer <kyle.thayer@gmail.com>
2024-04-27 16:46:28 -0700

    update fieldnames to be camel case and capitalize taxon names

--- separate_data.js
+++ separate_data.js
@@ -7,13 +7,49 @@ let taxon_list = []
 
 Object.keys(all_taxon).forEach(taxon_name => {
     let taxon_info = all_taxon[taxon_name]
-    taxon_info.name = taxon_name
-    let taxon_file_name = taxon_name.replaceAll(" ", "_") + ".json"
-    //console.log(taxon_name, taxon_info)
-    fs.writeFile("taxa/" + taxon_file_name, JSON.stringify(taxon_info))
+    
+    // add name
+    let capitalizedTaxonName = taxon_name[0].toUpperCase() + taxon_name.slice(1)
+    taxon_info.name = capitalizedTaxonName
+    
+    let taxon_file_name = taxon_name + ".json"
 
-    taxon_parent_summary[taxon_name] = taxon_info["parent_taxon"]
-    taxon_list.push(taxon_name)
+    // fix underscore fields
+    taxon_info.parentTaxon = taxon_info.parent_taxon
+    delete taxon_info.parent_taxon
+
+    taxon_info.popularSubtaxa = taxon_info.popular_subtaxa
+    delete taxon_info.popular_subtaxa
+
+    taxon_info.exampleMember = taxon_info.example_member
+    delete taxon_info.example_member
+
+    taxon_info.exampleMemberType = taxon_info.example_member_type
+    delete taxon_info.example_member_type
+
+    taxon_info.taxonomicRank = taxon_info.taxonomic_rank
+    delete taxon_info.taxonomic_rank
+
+    taxon_info.scientificName = taxon_info.scientific_name
+    delete taxon_info.scientific_name
+
+    taxon_info.otherNames = taxon_info.other_names
+    delete taxon_info.other_names
+
+    taxon_info.wikipediaImg = taxon_info.wikipedia_img
+    delete taxon_info.wikipedia_img
+
+    taxon_info.wikipediaPage = taxon_info.wikipedia_page
+    delete taxon_info.wikipedia_page
+    
+    taxon_info.popularAncestors = taxon_info.popular_ancestors
+    delete taxon_info.popular_ancestors
+
+    //console.log(taxon_name, taxon_info)
+    fs.writeFile("taxa_processed/" + taxon_file_name, JSON.stringify(taxon_info))
+
+    taxon_parent_summary[capitalizedTaxonName] = taxon_info["parentTaxon"]
+    taxon_list.push(capitalizedTaxonName)
 })
 
 fs.writeFile("taxon_parent_summary.json", JSON.stringify(taxon_parent_summary))
