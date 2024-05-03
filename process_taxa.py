ac6757f2d110b1a432dd0acade238e24462e0c75
Kyle <kmthayer@uw.edu>
2024-05-03 11:49:41 -0700

    make process_taxa get data from the taxa_source

--- process_taxa.py
+++ process_taxa.py
@@ -1,6 +1,39 @@
 import json, os
 
+
+# copy any changes from taxa_source to taxa_processed
+taxaSourceFiles = os.listdir("taxa_source")
+
+for taxonSourceFile in taxaSourceFiles:
+    print("copying source from " + taxonSourceFile)
+    f = open("taxa_source/" + taxonSourceFile, encoding="utf-8")
+    taxonSourceInfo = json.loads(f.read())
+
+    taxonProcessedInfo = {}
+    if(os.path.isfile("taxa_processed/"+ taxonSourceFile)):
+        f = open("taxa_processed/"+ taxonSourceFile, encoding="utf-8")
+        taxonProcessedInfo = json.loads(f.read())
+
+    anyChanges = False
+    for key, value in taxonSourceInfo.items():
+        if(taxonProcessedInfo[key] != value):
+            taxonProcessedInfo[key] = value
+            anyChanges = True
+    
+    if(anyChanges):
+        taxonProcessedInfo["needs_to_be_processed"] = True
+
+        print("saving updated version of file " + taxonSourceFile)
+        taxonInfoString = json.dumps(taxonSourceInfo, separators=(',', ':'))
+        f = open("taxa_processed/" + taxonSourceFile, "w", encoding="utf-8")
+        f.write(taxonInfoString)
+        f.close()
+
+
+
 # load all data
+# TODO: Use ["needs_to_be_processed"] to skip ones that aren't changed or don't need to be processed
+
 taxaInfo = {}
 
 taxaFiles = os.listdir("taxa_processed")
