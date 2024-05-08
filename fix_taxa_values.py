3a8b442689fbf7c1d3c3470023862e91e525177c
Kyle <kmthayer@uw.edu>
2024-05-07 19:14:16 -0700

    Add popular ancestor popularities to process_taxa (since they are needed for tracking where updates are needed)

--- fix_taxa_values.py
+++ fix_taxa_values.py
@@ -3,43 +3,84 @@
 
 import json, os
 
+
+####################################3
+# Add popularAncestorPops
+
+taxaProcessedFiles = os.listdir("docs/taxa_processed")
+
+taxaInfo = {}
+counter = 0
+
+for taxonFile in taxaProcessedFiles:
+    if(counter % 1000 == 0):
+        print("loading for processing" + taxonFile)
+    counter += 1
+    
+    f = open("docs/taxa_processed/" + taxonFile, encoding="utf-8")
+    taxonInfo = json.loads(f.read())
+    taxaInfo[taxonInfo["name"].lower()] = taxonInfo 
+
+for taxonName in taxaInfo.keys():
+    taxonInfo = taxaInfo[taxonName]
+    popularAncestorPops = [None, None, None, None]
+    if("popularAncestors" not in taxonInfo):
+        print("could not find popularAncestors in " + str(taxonName))
+        taxonInfo["popularAncestors"] = [None, None, None, None]
+        taxonInfo["needs_to_be_processed"] = True
+    else:
+        for i, popAncestor in enumerate(taxonInfo["popularAncestors"]):
+            if(popAncestor):
+                if(popAncestor.lower() in taxaInfo): 
+                    popularAncestorPops[i] = taxaInfo[popAncestor.lower()]["popularity"]
+                else: 
+                    popularAncestorPops[i] = None
+                    taxonInfo["needs_to_be_processed"] = True
+            else:
+                popularAncestorPops[i] = None
+    
+    taxonInfo["popularAncestorPops"] = popularAncestorPops
+
+    with open("docs/taxa_processed/" + taxonName.lower() + ".json", 'w') as f:
+        json.dump(taxonInfo, f, indent=0)
+
+
+##############################
 # fix http:// => https:// in wikipediaImgs
 
-taxaSourceFiles = os.listdir("docs/taxa_source")
-counter = 0
+# taxaSourceFiles = os.listdir("docs/taxa_source")
+# counter = 0
 
-for taxonSourceFile in taxaSourceFiles:
+# for taxonSourceFile in taxaSourceFiles:
     
-    if(counter % 1000 == 0):
-        print("copying over info for taxon " + taxonSourceFile)
-    counter += 1
+#     if(counter % 1000 == 0):
+#         print("copying over info for taxon " + taxonSourceFile)
+#     counter += 1
 
-    f = open("docs/taxa_source/" + taxonSourceFile, encoding="utf-8")
-    taxonSourceInfo = json.loads(f.read())
+#     f = open("docs/taxa_source/" + taxonSourceFile, encoding="utf-8")
+#     taxonSourceInfo = json.loads(f.read())
 
-    if("http://" in taxonSourceInfo["wikipediaImg"]):
-        taxonSourceInfo["wikipediaImg"] = taxonSourceInfo["wikipediaImg"].replace("http://", "https://")
+#     if("http://" in taxonSourceInfo["wikipediaImg"]):
+#         taxonSourceInfo["wikipediaImg"] = taxonSourceInfo["wikipediaImg"].replace("http://", "https://")
 
-    with open("docs/taxa_source/" + taxonSourceFile, 'w') as f:
-        json.dump(taxonSourceInfo, f, indent=0)
+#     with open("docs/taxa_source/" + taxonSourceFile, 'w') as f:
+#         json.dump(taxonSourceInfo, f, indent=0)
 
-counter = 0
-for taxonSourceFile in taxaSourceFiles:
+# counter = 0
+# for taxonSourceFile in taxaSourceFiles:
     
-    if(counter % 1000 == 0):
-        print("copying over info for taxon " + taxonSourceFile)
-    counter += 1
+#     if(counter % 1000 == 0):
+#         print("copying over info for taxon " + taxonSourceFile)
+#     counter += 1
 
-    f = open("docs/taxa_processed/" + taxonSourceFile, encoding="utf-8")
-    taxonSourceInfo = json.loads(f.read())
+#     f = open("docs/taxa_processed/" + taxonSourceFile, encoding="utf-8")
+#     taxonSourceInfo = json.loads(f.read())
 
-    if("http://" in taxonSourceInfo["wikipediaImg"]):
-        taxonSourceInfo["wikipediaImg"] = taxonSourceInfo["wikipediaImg"].replace("http://", "https://")
+#     if("http://" in taxonSourceInfo["wikipediaImg"]):
+#         taxonSourceInfo["wikipediaImg"] = taxonSourceInfo["wikipediaImg"].replace("http://", "https://")
 
-    with open("docs/taxa_processed/" + taxonSourceFile, 'w') as f:
-        json.dump(taxonSourceInfo, f, indent=0)
-
-
+#     with open("docs/taxa_processed/" + taxonSourceFile, 'w') as f:
+#         json.dump(taxonSourceInfo, f, indent=0)
 
 
 #######################################33
