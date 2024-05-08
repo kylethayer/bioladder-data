8daf33ec598665b539067e8667c3e02b07b2f9f6
Kyle <kmthayer@uw.edu>
2024-05-07 18:00:23 -0700

    fix wikipedia img links to be https

--- fix_taxa_values.py
+++ fix_taxa_values.py
@@ -3,9 +3,9 @@
 
 import json, os
 
-# make all json files have newlines (for checking value changes in git commits easier)
-taxaSourceFiles = os.listdir("docs/taxa_source")
+# fix http:// => https:// in wikipediaImgs
 
+taxaSourceFiles = os.listdir("docs/taxa_source")
 counter = 0
 
 for taxonSourceFile in taxaSourceFiles:
@@ -17,10 +17,13 @@ for taxonSourceFile in taxaSourceFiles:
     f = open("docs/taxa_source/" + taxonSourceFile, encoding="utf-8")
     taxonSourceInfo = json.loads(f.read())
 
+    if("http://" in taxonSourceInfo["wikipediaImg"]):
+        taxonSourceInfo["wikipediaImg"] = taxonSourceInfo["wikipediaImg"].replace("http://", "https://")
+
     with open("docs/taxa_source/" + taxonSourceFile, 'w') as f:
         json.dump(taxonSourceInfo, f, indent=0)
 
-
+counter = 0
 for taxonSourceFile in taxaSourceFiles:
     
     if(counter % 1000 == 0):
@@ -30,11 +33,48 @@ for taxonSourceFile in taxaSourceFiles:
     f = open("docs/taxa_processed/" + taxonSourceFile, encoding="utf-8")
     taxonSourceInfo = json.loads(f.read())
 
+    if("http://" in taxonSourceInfo["wikipediaImg"]):
+        taxonSourceInfo["wikipediaImg"] = taxonSourceInfo["wikipediaImg"].replace("http://", "https://")
+
     with open("docs/taxa_processed/" + taxonSourceFile, 'w') as f:
         json.dump(taxonSourceInfo, f, indent=0)
 
 
 
 
+#######################################33
+# make all json files have newlines (for checking value changes in git commits easier)
+# taxaSourceFiles = os.listdir("docs/taxa_source")
 
-# fix http:// => https:// in wikipediaImgs
+# counter = 0
+
+# for taxonSourceFile in taxaSourceFiles:
+    
+#     if(counter % 1000 == 0):
+#         print("copying over info for taxon " + taxonSourceFile)
+#     counter += 1
+
+#     f = open("docs/taxa_source/" + taxonSourceFile, encoding="utf-8")
+#     taxonSourceInfo = json.loads(f.read())
+
+#     with open("docs/taxa_source/" + taxonSourceFile, 'w') as f:
+#         json.dump(taxonSourceInfo, f, indent=0)
+
+
+# for taxonSourceFile in taxaSourceFiles:
+    
+#     if(counter % 1000 == 0):
+#         print("copying over info for taxon " + taxonSourceFile)
+#     counter += 1
+
+#     f = open("docs/taxa_processed/" + taxonSourceFile, encoding="utf-8")
+#     taxonSourceInfo = json.loads(f.read())
+
+#     with open("docs/taxa_processed/" + taxonSourceFile, 'w') as f:
+#         json.dump(taxonSourceInfo, f, indent=0)
+
+
+
+
+
+
