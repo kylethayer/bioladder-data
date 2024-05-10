2df1386266b3e627ca05dd8dd47e9340a24317dc
Kyle Thayer <kyle.thayer@gmail.com>
2024-05-09 23:35:47 -0700

    improve audit_taxa.py to work around rate limits

--- audit_taxa.py
+++ audit_taxa.py
@@ -10,7 +10,7 @@ import json, os, requests, time
 # Load all source taxa
 taxaInfo = {}
 
-taxaFiles = os.listdir("docs/taxa_source")
+taxaFiles = os.listdir("docs/taxa_processed")
 
 counter = 0
 for taxonFile in taxaFiles:
@@ -18,7 +18,7 @@ for taxonFile in taxaFiles:
         print("loading for processing" + taxonFile)
     counter += 1
     
-    f = open("docs/taxa_source/" + taxonFile, encoding="utf-8")
+    f = open("docs/taxa_processed/" + taxonFile, encoding="utf-8")
     taxonInfo = json.loads(f.read())
     taxaInfo[taxonInfo["name"].lower()] = taxonInfo 
 
@@ -38,6 +38,34 @@ for taxonName in taxaInfo:
 
 
 #### check wikipedia images
+
+## Mark all taxon as needing to be have their images checked
+
+# taxaProcessedFiles = os.listdir("docs/taxa_processed")
+
+# taxaInfo = {}
+# counter = 0
+
+
+# counter = 0
+# for taxonProcessedFile in taxaProcessedFiles:
+    
+#     if(counter % 1000 == 0):
+#         print("copying over info for taxon " + taxonProcessedFile)
+#     counter += 1
+
+#     f = open("docs/taxa_processed/" + taxonProcessedFile, encoding="utf-8")
+#     taxonProcessedInfo = json.loads(f.read())
+
+#     taxonProcessedInfo["needs_wiki_img_checked"] = True
+
+#     with open("docs/taxa_processed/" + taxonProcessedFile, 'w', encoding="utf-8") as f:
+#         json.dump(taxonProcessedInfo, f, separators=(',', ':'), indent=0, ensure_ascii=False)
+
+
+
+## check all marked taxa for their wiki images
+
 counter = 0
 # Find all subtaxa
 for taxonName in taxaInfo:
@@ -48,21 +76,42 @@ for taxonName in taxaInfo:
     taxonInfo = taxaInfo[taxonName.lower()]
     wikiImage = taxonInfo["wikipediaImg"]
     if(wikiImage):
-        try:
-            r = requests.get(wikiImage, timeout = 5)
-            if(r.status_code > 200):
-                time.sleep(5) # retry one after one second
+        if "needs_wiki_img_checked" in taxonInfo:
+            imgSucceeded = True
+            try:
                 r = requests.get(wikiImage, timeout = 5)
+                
                 if(r.status_code > 200):
-                    time.sleep(10) # retry 2 after three seconds
+                    time.sleep(10) # retry one after one second
                     r = requests.get(wikiImage, timeout = 5)
                     if(r.status_code > 200):
-                        print("*** Error loading '" + taxonName + "' (" + str(r.status_code) + ")")
-                        print("   - url: "+ wikiImage)
-                        if(r.status_code == 403):
-                            time.sleep(60)
-            time.sleep(1)
-            
-        except requests.exceptions.RequestException as e:
-            print("error loading image for " + taxonName)
-            print (e)
+                        time.sleep(30) # retry 2 after three seconds
+                        r = requests.get(wikiImage, timeout = 5)
+                        if(r.status_code > 200):
+                            print("*** Error loading '" + taxonName + "' (" + str(r.status_code) + ")")
+                            print("   - url: "+ wikiImage)
+                            imgSucceeded = False
+                            if(r.status_code == 403):
+                                time.sleep(60)
+                time.sleep(7.5) # 500 requests per hour (this is 480 per hour)
+                
+            except requests.exceptions.RequestException as e:
+                print("error loading image for " + taxonName)
+                print (e)
+                imgSucceeded = False
+            if imgSucceeded:
+                # No wiki image, mark as done
+                if("needs_wiki_img_checked" in taxonInfo):
+                    print("marking taxon " + taxonName + " as complete (image succeeded)")
+                    del taxonInfo["needs_wiki_img_checked"]
+
+                    with open("docs/taxa_processed/" + taxonName.lower() + ".json", 'w', encoding="utf-8") as f:
+                        json.dump(taxonInfo, f, separators=(',', ':'), indent=0, ensure_ascii=False)
+    else:
+        # No wiki image, mark as done
+        if("needs_wiki_img_checked" in taxonInfo):
+            print("marking taxon " + taxonName + " as complete (no image)")
+            del taxonInfo["needs_wiki_img_checked"]
+
+            with open("docs/taxa_processed/" + taxonName.lower() + ".json", 'w', encoding="utf-8") as f:
+                json.dump(taxonInfo, f, separators=(',', ':'), indent=0, ensure_ascii=False)
